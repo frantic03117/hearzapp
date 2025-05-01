a87bac178fd63fd5210a1dcf90bbd812f8647148
frantic03117 <shahrukh@franticpro.com>
2025-05-01 18:53:53 +0530

    updated

--- src/models/Banner.js
+++ src/models/Banner.js
@@ -1,10 +1,20 @@
 const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
+
     type: {
         type: String
     },
+    order: {
+        type: Number
+    },
     image: {
+        type: String
+    },
+    heading: {
+        type: String
+    },
+    short_description: {
         type: String
     }
 }, { timestamps: true });
