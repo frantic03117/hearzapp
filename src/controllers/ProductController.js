366d64df14b6f80550686b67384e217e3f602236
frantic03117 <shahrukh@franticpro.com>
2025-05-08 12:07:33 +0530

    updated

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -92,7 +92,7 @@ exports.getProducts = async (req, res) => {
         const totalDocs = await Product.countDocuments(fdata);
         const totalPages = Math.ceil(totalDocs / perPage);
         const skip = (page - 1) * perPage;
-        const products = await Product.find(fdata).populate("category").sort({ createdAt: -1 }).skip(skip).limit(perPage);
+        const products = await Product.find(fdata).populate("category").sort({ createdAt: -1 }).skip(skip).limit(perPage).lean();
         const wishlited = await Cart.find({ user: req.user._id, cart_status: "Cart" });
         const wishpids = wishlited.map(itm => itm.product.toString());
         const productsWithWishlist = products.map(product => ({
