2eb3b2aa81cf619b11e0286044eb46d104d94a6f
frantic03117 <shahrukh@franticpro.com>
2025-05-08 12:06:26 +0530

    updated

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { getProducts, createProduct, deleteProduct } = require("../controllers/ProductController");
 const store = require("../middleware/Upload");
+const { Auth } = require("../middleware/Auth");
 
 const router = Router();
-router.get('/', getProducts);
-router.post('/', store.any(), createProduct);
-router.delete('/delete/:id', deleteProduct);
+router.get('/', Auth, getProducts);
+router.post('/', Auth, store.any(), createProduct);
+router.delete('/delete/:id', Auth, deleteProduct);
 module.exports = router;
